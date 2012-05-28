d7860b5f596a0ce25cbb811cacaf2d9b76a1cba9
Nadia <sebeth@gmail.com>
2012-05-28 19:32:25 +0200

    reparing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ FirstApp::Application.routes.draw do
 
   root to: 'static_pages#home'
 
- 
+  get "static_pages/home"
 
- # get "users/new"
+  get "users/new"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+
 
 
 match '/signup', to: 'users#new'
